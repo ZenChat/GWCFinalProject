196d6d35f5a64e5080b8115b2581e573014ce174
ZenChat <41024114+ZenChat@users.noreply.github.com>
2018-07-11 11:28:39 -0400

    Add files via upload

--- template.js
+++ template.js
@@ -23,12 +23,35 @@ function draw() {
 }
 setInterval(draw, 10);
 
-// button
-function myFunction() {
-    var x = document.getElementById("myDIV");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
-}
+
+//
+// // dropdown
+// /* When the user clicks on the button,
+// toggle between hiding and showing the dropdown content */
+// function myFunction() {
+//     document.getElementById("amusement.html").classList.toggle("show");
+// }
+//
+// // Close the dropdown if the user clicks outside of it
+// window.onclick = function(e) {
+//   if (!e.target.matches('.dropbtn')) {
+//     var myDropdown = document.getElementById("Amusement");
+//       if (myDropdown.classList.contains('show')) {
+//         myDropdown.classList.remove('show');
+//       }
+//   }
+// }/* When the user clicks on the button,
+// toggle between hiding and showing the dropdown content */
+// function myFunction() {
+//     document.getElementById("Amusement").classList.toggle("show");
+// }
+//
+// // Close the dropdown if the user clicks outside of it
+// window.onclick = function(e) {
+//   if (!e.target.matches('.dropbtn')) {
+//     var myDropdown = document.getElementById("Amusement");
+//       if (myDropdown.classList.contains('show')) {
+//         myDropdown.classList.remove('show');
+//       }
+//   }
+// }
